da075a6f36511ad73f96c47fb55590a6faa01fbf
Donya <dramezan@usc.edu>
2021-08-19 20:38:25 -0700

    generate nodes of orion geometry txt file

--- Cases/2D_Orion/geometry/generate_fluent_geometry.py
+++ Cases/2D_Orion/geometry/generate_fluent_geometry.py
@@ -107,13 +107,81 @@ def main(argv=None):
 
   print("Options", mesh_options)
   point_A = np.array(mesh_options["geometry"]["origin"])
-  print("point_A", point_A)
+  point_B_prime = point_A + [mesh_options["geometry"]["length_AB"], 0,0]
+  point_C1_prime = point_B_prime + [-mesh_options["geometry"]["length_BC_h"], \
+                     mesh_options["geometry"]["length_BC_v"] ,0]
+  point_C2_prime = point_B_prime + [-mesh_options["geometry"]["length_BC_h"], \
+                     - mesh_options["geometry"]["length_BC_v"] ,0]
+  point_D1_prime = point_B_prime + [-mesh_options["geometry"]["length_BD_h"], \
+                     mesh_options["geometry"]["length_BD_v"] ,0]
+  point_D2_prime = point_B_prime + [-mesh_options["geometry"]["length_BD_h"], \
+                     - mesh_options["geometry"]["length_BD_v"] ,0]
+  point_E1_prime = point_A + [0, mesh_options["geometry"]["length_AE"],0]
+  point_E2_prime = point_A + [0, -mesh_options["geometry"]["length_AE"],0]
 
-  # Define coordinates of points (A,B,C,...)
-  
+  alpha = mesh_options["geometry"]["angle_of_attack"] * np.pi/180
+  point_B = np.zeros([3])
+  point_C1 = np.zeros([3])
+  point_C2 = np.zeros([3])
+  point_D1 = np.zeros([3])
+  point_D2 = np.zeros([3])
+  point_E1 = np.zeros([3])
+  point_E2 = np.zeros([3])
+  point_B[0] = point_B_prime[0]*np.cos(alpha) - \
+                 point_B_prime[1]*np.sin(alpha)
+  point_B[1] = point_B_prime[0]*np.sin(alpha) + \
+                 point_B_prime[1]*np.cos(alpha)
+  point_C1[0] = point_C1_prime[0]*np.cos(alpha) - \
+                 point_C1_prime[1]*np.sin(alpha)
+  point_C1[1] = point_C1_prime[0]*np.sin(alpha) + \
+                 point_C1_prime[1]*np.cos(alpha)
+  point_C2[0] = point_C2_prime[0]*np.cos(alpha) - \
+                 point_C2_prime[1]*np.sin(alpha)
+  point_C2[1] = point_C2_prime[0]*np.sin(alpha) + \
+                 point_C2_prime[1]*np.cos(alpha)
+  point_D1[0] = point_D1_prime[0]*np.cos(alpha) - \
+                 point_D1_prime[1]*np.sin(alpha)
+  point_D1[1] = point_D1_prime[0]*np.sin(alpha) + \
+                 point_D1_prime[1]*np.cos(alpha)
+  point_D2[0] = point_D2_prime[0]*np.cos(alpha) - \
+                 point_D2_prime[1]*np.sin(alpha)
+  point_D2[1] = point_D2_prime[0]*np.sin(alpha) + \
+                 point_D2_prime[1]*np.cos(alpha)
+  point_E1[0] = point_E1_prime[0]*np.cos(alpha) - \
+                 point_E1_prime[1]*np.sin(alpha)
+  point_E1[1] = point_E1_prime[0]*np.sin(alpha) + \
+                 point_E1_prime[1]*np.cos(alpha)
+  point_E2[0] = point_E2_prime[0]*np.cos(alpha) - \
+                 point_E2_prime[1]*np.sin(alpha)
+  point_E2[1] = point_E2_prime[0]*np.sin(alpha) + \
+                 point_E2_prime[1]*np.cos(alpha)
 
+ # creating dictionary of all the  8 points to store all the point in a sequence
+#  probe_points = {"p1" : point_A, "p2": point_E1, "p3": point_D1, \
+#                   "p4": point_C1, "p5": point_B, "p6": point_C2, \
+#                     "p7": point_D2, "p8": point_E2} 
 
+  num_control_point = 8
+  num_dim = 3
+  probe_points = np.zeros([num_control_point, num_dim])
+  probe_points[0,:] = point_A
+  probe_points[1,:] = point_E1
+  probe_points[2,:] = point_D1
+  probe_points[3,:] = point_C1
+  probe_points[4,:] = point_B
+  probe_points[5,:] = point_C2
+  probe_points[6,:] = point_D2
+  probe_points[7,:] = point_E2
+ 
   # Write output file
+  file = open("orion_geometry.txt", "w")
+  file.write("blobk"+ "  "+ "pintID"+ "  "+ "x" +"  "+ "y" + "  "+ "z"+ "\n")
+  for i_point in range(num_control_point):
+      file.write(str(1) + "  " + str(i_point+1) + "  " +
+                  str(probe_points[i_point, 0])+ "  " + \
+                   str(probe_points[i_point, 1])+ "  " + \
+                     str(probe_points[i_point,2])+ "\n")
+
 
 #------------------------------------------------------------------------------
 if __name__ == "__main__":
